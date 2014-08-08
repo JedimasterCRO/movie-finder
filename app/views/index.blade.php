017e44285c285893263ab7d1d6b3a37d9c9848f6
JedimasterCRO <goran.jedimaster@gmail.com>
2014-08-08 11:32:53 +0200

    init
    init

---
+++ app/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<meta charset="UTF-8">
+	{{HTML::style('css/bootstrap.min.css')}}
+	{{HTML::style('css/main.css')}}
+</head>
+<body class="bg-default">
+	<div class="navbar navbar-default navbar-static-top">
+		<center><a href="#"><img class="logo" src="img/mf_logo.png" alt="logo"></a></center>
+	</div>
+	</div>
+		<div class="row col-bborder">
+			<div class="col-md-9 col-rborder"><h1>Naslov 1</h1>
+			<p>
+				<a href="#">Lorem ipsum</a> dolor sit amet, consectetur adipiscing elit. Sed accumsan purus sed porttitor suscipit. Nam viverra nisi sit amet enim auctor, et pulvinar tellus faucibus. Donec ultricies, turpis eu eleifend scelerisque, lectus arcu elementum magna, nec blandit erat augue eget libero. Suspendisse venenatis, ligula id congue egestas, metus metus dapibus ligula, nec eleifend metus sem vitae diam. Vivamus quis varius mi. Curabitur nec congue risus. Pellentesque accumsan faucibus adipiscing. Suspendisse sit amet neque et neque euismod malesuada a quis augue. Vestibulum ultrices augue at congue elementum.
+
+				Nulla aliquet turpis eget volutpat blandit. Sed a ipsum sollicitudin, tristique dui eu, congue eros. Duis non sapien in nisl scelerisque feugiat. Aliquam erat volutpat. Mauris pellentesque leo et dui faucibus commodo. Cras rutrum, tellus eu accumsan feugiat, nisi tortor placerat magna, in consectetur nunc elit eu tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie lectus purus, sit amet sollicitudin eros varius nec. Donec commodo luctus pretium. Nulla luctus sapien vel sapien semper convallis. Suspendisse potenti. Nam ac scelerisque lorem, convallis tristique lectus. Nam congue turpis nisl, quis semper quam varius eu.
+
+				Nam congue ante sed aliquet commodo. Sed molestie ligula nec gravida tincidunt. Mauris vehicula nibh molestie ornare pharetra. Donec lacinia venenatis ipsum, quis sagittis lacus ultrices nec. Phasellus bibendum quam eu ultrices pharetra. Sed vestibulum mauris vitae convallis tincidunt. Proin vehicula nunc id tempus dignissim. Donec vitae condimentum arcu. In volutpat aliquet aliquet. Sed posuere vel lectus vitae interdum. Aenean nec fermentum purus. Integer pellentesque egestas arcu non condimentum. Maecenas non purus id enim sollicitudin auctor gravida eget purus.
+
+				Suspendisse potenti. Aenean tempus vitae nulla vitae vulputate. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut et egestas tortor. Integer sagittis et mi vitae viverra. Curabitur enim urna, pulvinar rutrum ipsum ut, molestie ultricies dolor. Vivamus sit amet gravida elit, id ultrices nunc. Duis molestie, nisi sed gravida dignissim, dui magna sollicitudin ligula, ut commodo odio enim non libero. In mollis libero diam, quis varius arcu hendrerit vel. Fusce feugiat augue in venenatis fringilla. Fusce augue mi, eleifend a quam eu, convallis interdum lectus. Morbi placerat bibendum ultrices. Curabitur lacinia diam eu porttitor elementum. Phasellus tristique turpis in pharetra varius. Quisque feugiat magna vel enim porttitor, et vulputate eros sollicitudin.
+
+				Nullam commodo elementum diam nec varius. Nulla facilisi. Aliquam tincidunt malesuada molestie. Ut eget blandit eros. Suspendisse at elit fringilla, mollis velit a, iaculis mauris. Nulla nec dictum mi. Curabitur neque tellus, adipiscing sit amet eros non, ornare consectetur leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent porttitor lobortis enim et mollis. Duis iaculis ante ullamcorper, cursus massa vulputate, convallis sapien. In iaculis augue et mauris aliquam bibendum.
+			</p>
+			</div>
+			<div class="col-md-2">Top lista</div>
+		</div>
+		<div class="navbar"></div>
+</body>
+</html>
